32a54cfe5e488e8289d108cd9a4ad889f0825f87
André Borges <andreborgescastro@gmail.com>
2025-10-31 23:37:36 -0300

    configures sqlite and typeorm. Also includes db load file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit, Logger } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      synchronize: true,
+      autoLoadEntities: true, // carrega entidades registradas nos módulos
+      logging: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  private readonly logger = new Logger(AppModule.name);
+  async onModuleInit() {
+    this.logger.log('(SQLite ready)');
+  }
+}
